8a08aba3e8f606ef901c2a3c8426ed52880c0e09
Khionu Sybiern <dev@khionu.net>
2023-10-04 14:00:40 -0700

    fix: add missing semicolon

--- flake.nix
+++ flake.nix
@@ -1,12 +1,30 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.disko = {
+    url = "github:nix-community/disko";
+    follows = inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = { ... }: {
-    nixosModules.fvtt-multitenant = { config, lib, ... }: {
+  outputs = { nixpkgs, disko, ... }: {
+    nixosModules.aio = { config, lib, ... }: {
       options.fvtt-multitenant.enable =
         lib.mkEnableOption "enable Khionu's Multitenant Foundry setup";
 
       config = lib.mkIf config.fvtt-multitenant.enable {
+        services.nomad.enable = true;
+        services.nomad = {
+          enable = true;
+          extraSettingsPlugins = [
+            pkgs.nomad-driver-podman
+          ];
+          settings = {
+            server = {
+              enabled = true;
+              bootstrap_expect = 1;
+            };
+            client.enabled = true;
+          };
+        };
       };
     };
   };
